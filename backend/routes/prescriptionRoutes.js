eaea09317e3bc3aff7481de8882c5e506932e634
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-06-26 00:47:55 +0530

    remove unused getPrescriptionByOpdId route from prescriptionRoutes

--- backend/routes/prescriptionRoutes.js
+++ backend/routes/prescriptionRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { getPrescriptions, getPrescriptionById, getPrescriptionByOpdId, createPrescription, dispensePrescription } = require('../controllers/prescriptionController');
+const { getPrescriptions, getPrescriptionById, createPrescription, dispensePrescription } = require('../controllers/prescriptionController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', protect, getPrescriptions);
 router.get('/:id', protect, getPrescriptionById);
-router.get('/opd/:opd_registration_id', protect, getPrescriptionByOpdId);
 router.post('/', protect, authorize('Admin', 'Doctor'), createPrescription);
 router.post('/:id/dispense', protect, authorize('Admin', 'Pharmacist'), dispensePrescription);
 
